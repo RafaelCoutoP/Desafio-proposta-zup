cee6065dad07e88c38dace0b26501f9979884b80
Rafael Couto <rafaelcouto@MacBook-Air-de-Rafael.local>
2021-11-29 15:54:35 -0300

    Setup Inicial

---
+++ src/test/java/br/com/rafacdev/desafioproposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafacdev.desafioproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
